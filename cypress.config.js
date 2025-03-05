04d5d1c4d153b09afc4a58a5b76d80657a555e72
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2025-03-05 13:24:53 +0100

    mise à jour du jenkinsfile

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
-const { defineConfig } = require('cypress');
-
+const { defineConfig } = require("cypress");
+const grep = require("@cypress/grep/src/plugin");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter', // a ajouter
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/my-test-output-[hash].xml',},
   e2e: {
     setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config);
+      grep(on, config);
       require('cypress-mochawesome-reporter/plugin')(on);// a ajouter
-    },
+
+      return config;    },
   },
 });
